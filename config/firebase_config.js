732fdb327500f1492e71834fce74a57c8595acfc
Helio Junior <87399421+Hjr2411@users.noreply.github.com>
2025-09-10 19:49:38 -0300

    Update firebase_config.js

--- config/firebase_config.js
+++ config/firebase_config.js
@@ -1,19 +1,18 @@
-// config/firebase_config.js
+// /config/firebase_config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
-// 🔧 Cole o SEU config real aqui:
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
+  apiKey: "SUA_KEY",
   authDomain: "SEU_PROJETO.firebaseapp.com",
   projectId: "SEU_PROJETO",
   storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "NUMERO",
+  messagingSenderId: "NUM",
   appId: "APP_ID",
-  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com" // importante p/ RTDB
+  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com" // obrigatório p/ RTDB
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
+export const db = getFirestore(app); // opcional
